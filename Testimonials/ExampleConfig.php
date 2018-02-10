56a95fbefd8501ce5c2b2ea4d5f0300e6922308e
hannd17 <29674287+hannd17@users.noreply.github.com>
2018-02-10 13:40:23 +0000

    Update USAGE.md

--- Testimonials/ExampleConfig.php
+++ Testimonials/ExampleConfig.php
@@ -1,5 +1,8 @@
 <?php
 
+# This is the example configuration. 
+# Replace the values below to suit your website.
+
 define("ADMIN_EMAIL", "example@example.com"); # The email to receive testimonial approval requests.
 define("NOREPLY_EMAIL", "no-reply@example.com"); # The no-reply email that emails are sent from.
 define("WEB_ADDRESS", "http://example.com"); # Your website address, including http:// or https://
