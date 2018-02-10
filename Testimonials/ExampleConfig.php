5adc523a0eb3166625622fcb38a565e74740f8e4
hannd17 <29674287+hannd17@users.noreply.github.com>
2018-02-10 13:23:16 +0000

    Add example Config.php

---
+++ Testimonials/ExampleConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+define("ADMIN_EMAIL", "example@example.co.uk");
+define("ADMIN_NAME", "Admin");
+define("NOREPLY_EMAIL", "example@example.co.uk");
+define("WEB_ADDRESS", "http://localhost");
+
+define("LOCAL_DEBUG", false);
+
+define("ENABLE_MAIL", false);
+
+?>
