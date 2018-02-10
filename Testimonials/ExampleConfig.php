dc9a76899d43a3ca398786b080393ce089260645
hannd17 <29674287+hannd17@users.noreply.github.com>
2018-02-10 13:28:05 +0000

    Update ExampleConfig.php

--- Testimonials/ExampleConfig.php
+++ Testimonials/ExampleConfig.php
@@ -1,11 +1,11 @@
 <?php
 
-define("ADMIN_EMAIL", "example@example.com");
-define("ADMIN_NAME", "Admin");
-define("NOREPLY_EMAIL", "example@example.com");
-define("WEB_ADDRESS", "http://example.com");
+define("ADMIN_EMAIL", "example@example.com"); # The email to receive testimonial approval requests.
+define("ADMIN_NAME", "Admin"); # Your name.
+define("NOREPLY_EMAIL", "example@example.com"); # THe no-reply email that emails are sent from.
+define("WEB_ADDRESS", "http://example.com"); # Your website address, including http:// or https://
 
-define("LOCAL_DEBUG", false);
+define("LOCAL_DEBUG", false); # Not currently used.
 
 define("ENABLE_MAIL", true); # Enable email sending. 
 
